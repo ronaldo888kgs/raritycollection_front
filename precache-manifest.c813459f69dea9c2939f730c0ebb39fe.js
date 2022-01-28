15c3fe3fb2848c81d3bce600588fd3f19ba982fa
ronaldo888kgs <ronaldo888kgs@gmail.com>
2022-01-29 01:18:18 +0800

    Updates

---
+++ precache-manifest.c813459f69dea9c2939f730c0ebb39fe.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e73897fcf507eb54c0ca81164003ade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f60636adfbc341b4608d",
+    "url": "/static/css/6.ffd85cc7.chunk.css"
+  },
+  {
+    "revision": "285d620ffba8fc0e505a",
+    "url": "/static/css/8.cd04230d.chunk.css"
+  },
+  {
+    "revision": "efb54f8b7ad3c44dafb0",
+    "url": "/static/js/0.8a3cabca.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/0.8a3cabca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28637e8b12f940c6f15f",
+    "url": "/static/js/1.c929191b.chunk.js"
+  },
+  {
+    "revision": "f619751ebbcf7d0c5daa",
+    "url": "/static/js/10.84bf85b4.chunk.js"
+  },
+  {
+    "revision": "4d635ea6a6ff64ae24ee",
+    "url": "/static/js/11.974e7cca.chunk.js"
+  },
+  {
+    "revision": "b8874fa32cbd2b94e01a",
+    "url": "/static/js/12.dd695a26.chunk.js"
+  },
+  {
+    "revision": "3ad47c523e5fdb8bcac3",
+    "url": "/static/js/13.e58f424a.chunk.js"
+  },
+  {
+    "revision": "0a249dfc2f9da868957b",
+    "url": "/static/js/14.b7934f9e.chunk.js"
+  },
+  {
+    "revision": "31cb153d84bcb108e693",
+    "url": "/static/js/15.4d38439f.chunk.js"
+  },
+  {
+    "revision": "5bd89beb934fd59fd6f9",
+    "url": "/static/js/16.9bfd9bea.chunk.js"
+  },
+  {
+    "revision": "fa79bf8368af5db6f706",
+    "url": "/static/js/2.b111d832.chunk.js"
+  },
+  {
+    "revision": "54250fbc4240b1d39aad",
+    "url": "/static/js/3.a2e64569.chunk.js"
+  },
+  {
+    "revision": "f60636adfbc341b4608d",
+    "url": "/static/js/6.aad92af3.chunk.js"
+  },
+  {
+    "revision": "760fb1cfc98a247386f1afaec8b16d48",
+    "url": "/static/js/6.aad92af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36719b1c0cb1db62742",
+    "url": "/static/js/7.11dc515f.chunk.js"
+  },
+  {
+    "revision": "285d620ffba8fc0e505a",
+    "url": "/static/js/8.a518f202.chunk.js"
+  },
+  {
+    "revision": "d18253b199adb76f9c57",
+    "url": "/static/js/9.327ceb33.chunk.js"
+  },
+  {
+    "revision": "88a88da130a1eb696e05",
+    "url": "/static/js/main.c78c1159.chunk.js"
+  },
+  {
+    "revision": "9b8a9df33bb7ade3b079",
+    "url": "/static/js/runtime-main.f8f6ffcc.js"
+  },
+  {
+    "revision": "6db5f28180e7dab57d574b3ce5eed878",
+    "url": "/static/media/ErrorImage.6db5f281.svg"
+  },
+  {
+    "revision": "e407bf07c83b5f4a77c53d10999aa788",
+    "url": "/static/media/bsc.e407bf07.svg"
+  },
+  {
+    "revision": "0fb0ca238df6d754e77c37c5de685e1f",
+    "url": "/static/media/ethereum.0fb0ca23.svg"
+  },
+  {
+    "revision": "dcc2103ac6732d113be75a9d2018d128",
+    "url": "/static/media/metamask.dcc2103a.svg"
+  },
+  {
+    "revision": "106f27a7994e53f09eab64bbf9e23174",
+    "url": "/static/media/notfound.106f27a7.svg"
+  },
+  {
+    "revision": "8188d318efdd37ee7d7dfd9aa069c318",
+    "url": "/static/media/rarity.8188d318.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0bd30abf944737d24508cfcc7d8a27aa",
+    "url": "/static/media/walletConnect.0bd30abf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/raritycollection_front/precache-manifest.2b6185d71834e5ed625d8095841ca1a8.js"
+  "/precache-manifest.c813459f69dea9c2939f730c0ebb39fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raritycollection_front/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
